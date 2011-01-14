d0f7c2734e1459c2c3b1e16c429b6f7b76062e0e
Hoang Le <yugiohhh@gmail.com>
2011-01-14 08:31:33 +0700

    update common.inc

--- includes/common.inc.php
+++ includes/common.inc.php
@@ -1,6 +1,5 @@
 <?php
 require_once('../libs/Smarty.class.php');
-//require_once '../../yupro/discuz/include/common.inc.php';
 $smarty = new Smarty();
 
 //Global var config
@@ -22,12 +21,4 @@ $smarty->assign('yugeturi',$_GET['module']);
 
 $yusptime = time(); //start proccess
 $smarty->assign('yusptime',$yusptime);
-
-
-//$yuqr = $db->query("SELECT author,subject FROM {$tablepre}threads ORDER BY `tid` DESC LIMIT 20");
-//set_time_limit(0);
-//sleep(10);
-
-//while($yunewpost = $db->fetch_array($yuqr)) {print_r($yunewpost); echo"<br />";}
-
 ?>
